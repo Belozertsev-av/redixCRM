ed4b402f12d9c2aaa4a1769d8ce9b2daf13515a0
Belozertsev Andrew <65508634+Belozertsev-av@users.noreply.github.com>
2024-01-11 21:42:44 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,8 +39,7 @@ export default defineNuxtConfig({
         { rel: 'icon', href: '/redix_logo.png', type: 'image/png' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap' },
-        { rel: 'icon', href: '/redix_logo.png', type: 'image/png' }
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap' }
       ]
     }
   },
