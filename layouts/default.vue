17ad69db7ec12b7cd2d9d23dacb760c7694a34ce
belozertsev_aw <andrew.vl.belozertsev@gmail.com>
2024-01-13 22:41:49 +0300

    Рефакторинг

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,6 @@
 <template>
     <v-app>
         <header>
-           <!-- <v-app-bar :elevation="5">
-        
-            <ul>
-                <li>
-                    <NuxtLink to="/" class="a1"><v-btn>Авторизация</v-btn></NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/mainPage" class="a1"><v-btn>Главное окно</v-btn></NuxtLink>
-            </li>
-           </ul>
-        </v-app-bar> -->
         <MainHeader></MainHeader>
         <MainToolbar></MainToolbar>
         </header>
@@ -27,18 +16,7 @@
 </script>
 
 <style lang="scss" scoped>
-    ul{
-        display: flex;
-        justify-content: center;
+    header{
         width: 100%;
-    }
-    li{
-        margin: 0 30px;
-        list-style-type: none;
-    }
-    .a1{
-        text-decoration: none;
-        color: black;
-
     }
 </style> 
