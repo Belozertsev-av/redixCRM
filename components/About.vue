062dd4b21153f7d6c8694e89f36c14e4a7b3a23e
Bodi4ka0303 <Bodi4ka0303>
2024-01-13 20:11:53 +0300

    Ком

--- components/About.vue
+++ components/About.vue
@@ -1,16 +1,38 @@
 <template>
-    <div>
-        <h2>About</h2>
+    <div class="about-container">
+      <h2 class="about-title">♡</h2>
     </div>
-</template>
-
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-    h2 {
-        margin-top: 150px;
-        text-align: center;
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style lang="scss" scoped>
+    .about-container {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100vh; 
+      background: linear-gradient(to bottom, #3498db, #2c3e50); 
     }
-</style>
+  
+    .about-title {
+      margin-top: 0;
+      text-align: center;
+      color: #ecf0f1; 
+      font-size: 2em; 
+      animation: pulse 1.5s infinite; 
+    }
+  
+    @keyframes pulse {
+      0% {
+        transform: scale(1);
+      }
+      50% {
+        transform: scale(2);
+      }
+      100% {
+        transform: scale(4);
+      }
+    }
+  </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,40 +1,48 @@
 <template>
     <v-app>
-        <header>
-           <v-app-bar :elevation="5">
-            <ul>
-                <li>
-                    <NuxtLink to="/" class="a1"><v-btn>Авторизация</v-btn></NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/about" class="a1"><v-btn>Главное окно</v-btn></NuxtLink>
+      <header>
+        <v-app-bar :elevation="5">
+          <ul>
+            <li>
+              <NuxtLink to="/" class="nav-link"><v-btn>Главная</v-btn></NuxtLink>
             </li>
-           </ul>
+            <li>
+              <NuxtLink to="/about" class="nav-link"><v-btn>Настройки</v-btn></NuxtLink>
+            </li>
+          </ul>
         </v-app-bar>
-        </header>
-        <main>
-            <slot></slot>
-        </main>
+      </header>
+      <main>
+        <slot></slot>
+      </main>
     </v-app>
-</template>
-
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-    ul{
-        display: flex;
-        justify-content: center;
-        width: 100%;
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style lang="scss" scoped>
+    ul {
+      display: flex;
+      justify-content: left;
+      width: 100%;
+      background-color: #0059b3; 
+      padding: 15px 0; 
     }
-    li{
-        margin: 0 30px;
-        list-style-type: none;
+  
+    li {
+      margin: 0 30px;
+      list-style-type: none;
     }
-    .a1{
-        text-decoration: none;
-        color: black;
-
+  
+    .nav-link {
+      text-decoration: none;
+      color: #ecf0f1; 
+      transition: color 0.3s ease; 
+  
+      &:hover {
+        color: #72c2f8;
+      }
     }
-</style>
+    
+  </style>
