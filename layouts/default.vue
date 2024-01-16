0d72534ea93e54e0f28ce0d9ba208d0c8cdcccf9
Belozertsev Andrew <65508634+Belozertsev-av@users.noreply.github.com>
2024-01-16 21:11:37 +0300

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,16 +8,10 @@
         <main>
             <slot></slot>
         </main>
-        <footer>
-            <MainResults></MainResults>
-        </footer>
     </v-app>
 </template>
 
 <script setup>
-import MainResults from '~/components/MainResults.vue';
-
-
 </script>
 
 <style lang="scss" scoped>
