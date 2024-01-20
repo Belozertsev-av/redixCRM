f6c1c7c9c460336b49d63835c0698c3d83e0c209
belozertsev_aw <andrew.vl.belozertsev@gmail.com>
2024-01-20 15:26:32 +0300

    hotfix

--- components/Diagrams/MainDailyDiagram.vue
+++ components/Diagrams/MainDailyDiagram.vue
@@ -5,46 +5,14 @@
             <div class="daily-diagram__text">Необходимо в день</div>
             <div class="daily-diagram__gistogram-body">
                 <div class="daily-diagram__plan">
-                    <div class="daily-diagram__plan-text"
-                    :style="`top: calc(100% - ${dailyPercents.plan}% - 5px);`">Норма</div>
-                    <div class="daily-diagram__plan-line"
-                    :style="`top: calc(100% - ${dailyPercents.plan}%);`"></div>
+                    <div class="daily-diagram__plan-text" :style="`top: calc(100% - ${dailyPercents[0].data}% - 5px);`">Норма
+                    </div>
+                    <div class="daily-diagram__plan-line" :style="`top: calc(100% - ${dailyPercents[0].data}%);`"></div>
                 </div>
                 <div class="daily-diagram__gistogram">
-                    <div class="gistogram__item">
-                        <div class="gistogram__column" 
-                        :style="`height: ${dailyPercents.mon}%;`"></div>
-                        <div class="gistogram__text">Пн.</div>
-                    </div>
-                    <div class="gistogram__item">
-                        <div class="gistogram__column" 
-                        :style="`height: ${dailyPercents.tue}%;`"></div>
-                        <div class="gistogram__text">Вт.</div>
-                    </div>
-                    <div class="gistogram__item">
-                        <div class="gistogram__column" 
-                        :style="`height: ${dailyPercents.wed}%;`"></div>
-                        <div class="gistogram__text">Ср.</div>
-                    </div>
-                    <div class="gistogram__item">
-                        <div class="gistogram__column" 
-                        :style="`height: ${dailyPercents.thu}%;`"></div>
-                        <div class="gistogram__text">Чт.</div>
-                    </div>
-                    <div class="gistogram__item">
-                        <div class="gistogram__column" 
-                        :style="`height: ${dailyPercents.fri}%;`"></div>
-                        <div class="gistogram__text">Пт.</div>
-                    </div>
-                    <div class="gistogram__item">
-                        <div class="gistogram__column" 
-                        :style="`height: ${dailyPercents.sat}%;`"></div>
-                        <div class="gistogram__text">Сб.</div>
-                    </div>
-                    <div class="gistogram__item">
-                        <div class="gistogram__column" 
-                        :style="`height: ${dailyPercents.sun}%;`"></div>
-                        <div class="gistogram__text">Вс.</div>
+                    <div class="gistogram__item" v-for="item in dailyPercents.slice(1)" :key="item.id">
+                        <div class="gistogram__column" :style="`height: ${item.data}%; background-color: ${(item.data >= dailyPercents[0].data) ? '#1f93ff' : '#efeded'};`"></div>
+                        <div class="gistogram__text"> {{ item.name }}</div>
                     </div>
                 </div>
             </div>
@@ -79,18 +47,18 @@ const dayPlan = ref(Math.round(props.plan / 30))
 const max = ref(Math.max(...values))
 // computed свойства
 const dailyPercents = computed(() => {
-    if (max.value < dayPlan.value ) max.value = dayPlan.value
+    if (max.value < dayPlan.value) max.value = dayPlan.value
 
-    return {
-        plan: Math.round(100 + (dayPlan.value / max.value * 100 - 100)),
-        mon: Math.round(100 + (props.dailySum.mon / max.value * 100 - 100)),
-        tue: Math.round(100 + (props.dailySum.tue / max.value * 100 - 100)),
-        wed: Math.round(100 + (props.dailySum.wed / max.value * 100 - 100)),
-        thu: Math.round(100 + (props.dailySum.thu / max.value * 100 - 100)),
-        fri: Math.round(100 + (props.dailySum.fri / max.value * 100 - 100)),
-        sat: Math.round(100 + (props.dailySum.sat / max.value * 100 - 100)),
-        sun: Math.round(100 + (props.dailySum.sun / max.value * 100 - 100))
-    }
+    return [
+        { id: 0, name: 'План', data: Math.round(100 + (dayPlan.value / max.value * 100 - 100))},
+        { id: 1, name: 'Пн', data: Math.round(100 + (props.dailySum.mon / max.value * 100 - 100))},
+        { id: 2, name: 'Вт', data: Math.round(100 + (props.dailySum.tue / max.value * 100 - 100))},
+        { id: 3, name: 'Ср', data: Math.round(100 + (props.dailySum.wed / max.value * 100 - 100))},
+        { id: 4, name: 'Чт', data: Math.round(100 + (props.dailySum.thu / max.value * 100 - 100))},
+        { id: 5, name: 'Пт', data: Math.round(100 + (props.dailySum.fri / max.value * 100 - 100))},
+        { id: 6, name: 'Сб', data: Math.round(100 + (props.dailySum.sat / max.value * 100 - 100))},
+        { id: 7, name: 'Вс', data: Math.round(100 + (props.dailySum.sun / max.value * 100 - 100))}
+]
 })
 
 </script>
@@ -98,25 +66,30 @@ const dailyPercents = computed(() => {
 <style lang="scss" scoped>
 .daily-diagram {
     padding: $padding 0;
-	&__body {
+
+    &__body {
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: flex-start;
         width: 300px;
         padding: $paddingMedium;
-	}
-	&__title {
+    }
+
+    &__title {
         padding-bottom: $padding;
-	}
-	&__text {
+    }
+
+    &__text {
         padding-bottom: $padding * 2;
-	}
-    &__gistogram-body{
+    }
+
+    &__gistogram-body {
         width: 100%;
         display: flex;
     }
-	&__gistogram {
+
+    &__gistogram {
         width: 100%;
         height: 120px;
         margin-bottom: $paddingMedium;
@@ -125,21 +98,24 @@ const dailyPercents = computed(() => {
         justify-content: space-between;
         align-items: flex-end;
         position: relative;
-	}
+    }
+
     &__plan {
         width: 22.5%;
         height: 100px;
         position: relative;
         color: $unactiveFontColor;
         z-index: 2;
-	}
-	&__plan-text {
+    }
+
+    &__plan-text {
         transition: 0.2s;
         font-size: 14px;
         position: absolute;
         left: 0;
-	}
-	&__plan-line {
+    }
+
+    &__plan-line {
         transition: 0.2s;
         position: absolute;
         border-top: 2px dotted $unactiveFontColor;
@@ -147,30 +123,32 @@ const dailyPercents = computed(() => {
         width: 220px;
         left: 100%;
         top: 0%;
-	}
+    }
 }
+
 .gistogram {
-	&__item {
+    &__item {
         height: 100%;
         display: flex;
         flex-direction: column;
         justify-content: flex-end;
         position: relative;
-	}
-    &__text{
+    }
+
+    &__text {
         position: absolute;
         bottom: -18px;
         left: 3px;
         font-size: 14px;
         text-align: center;
     }
-	&__column {
+
+    &__column {
         transition: 0.2s;
         width: 23px;
         background-color: $primaryColor;
         border-top-left-radius: $radius;
         border-top-right-radius: $radius;
-	}
+    }
 }
-
 </style>
