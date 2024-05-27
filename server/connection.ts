af3d383b110bd717c002f501c705b8bb23a12863
Belozertsev-av <andrew.vl.belozertsev@gmail.com>
2024-05-27 19:39:49 +0300

    Made server side customers and employees

--- server/connection.ts
+++ server/connection.ts
@@ -1,11 +1,11 @@
-import {cert, getApps, initializeApp} from "firebase-admin/app";
+import {initializeApp, getApps, cert} from "firebase-admin/app";
 import {getFirestore} from "firebase-admin/firestore";
 
 const apps = getApps()
 
 if (!apps.length) {
     initializeApp({
-        credential: cert('./redix-fd7a9-firebase-adminsdk-v96db-e75f313bec.json')
+        credential: cert('./redix-fd7a9-firebase-adminsdk-v96db-04b3cea590.json')
     })
 }
 export const db = getFirestore()
--- server/model/commonTypes.ts
+++ server/model/commonTypes.ts
@@ -1,4 +1,31 @@
+import { Timestamp } from "firebase-admin/firestore"
+
 export type Damage = {
     uuid: string,
     damage: string
+}
+
+
+export type Customers =  {
+    uuid: string,
+    customer_last_name: string,
+    customer_first_name: string
+    customer_patronim: string
+    birthdate: Timestamp,
+    email: string,
+    phone: string
+
+
+}
+
+export type Employees =  {
+    uuid: string,
+    customer_last_name: string,
+    customer_first_name: string
+    customer_patronim: string
+    login: string,
+    password: string,
+    
+
+
 }
